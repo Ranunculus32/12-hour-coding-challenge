2071a81030178e2573b4c1eef3a9d56b20083db5
Christine Schale <108648777+Ranunculus32@users.noreply.github.com>
2023-11-23 14:18:40 +0100

    Preparing for backend functionalities
    - created login and register pages
    - updated the style
    - installed json, express, mongoose, dotenv, nodemon

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const stickyNav = document.querySelector("#navbarTop");
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+// Sticky navigation
+// const initialCoords = sticky.getBoundingClientRect();
+// console.log(initialCoords);
+
+// window.addEventListener("scroll", function (e) {
+//   console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) classList.add("sticky");
+//   else classList.remove("sticky");
+// });
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+    
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1 //this is 10%
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(stickyNav);
